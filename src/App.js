f656f2336e522329eca7ad2a204b4ac53dc96f4d
Ali <ali.mohammed4@yahoo.com>
2021-12-13 16:43:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Product from "../src/pages/productGatogary";
-import Home from "./pages/home";
-import ContactUs from "./pages/contactUS/";
-import Products from "./pages/Products";
-import AboutUS from "./pages/aboutUS/AboutUS";
-import Gallerys from "./pages/gallery";
-import NotFoundPage from "./pages/pageNotFound/";
-import ProductDetails from "./pages/productDetails/";
+// import Product from "../src/pages/productGatogary";
+// import Home from "./pages/home";
+// import ContactUs from "./pages/contactUS/";
+// import Products from "./pages/Products";
+// import AboutUS from "./pages/aboutUS/AboutUS";
+// import Gallerys from "./pages/gallery";
+// import NotFoundPage from "./pages/pageNotFound/";
+// import ProductDetails from "./pages/productDetails/";
 import { Footer, Header } from "./components/layouts";
+import { Suspense, lazy } from "react";
 
 // const NavRoute = ({ exact, path, component: Component }) => (
 //   <Route
@@ -24,12 +25,30 @@ import { Footer, Header } from "./components/layouts";
 //   />
 // );
 
+// const NavBar = lazy(() => import("./components/layouts/header/Header.jsx"));
+
+const Home = lazy(() => import("./pages/home"));
+
+const Product = lazy(() => import("../src/pages/productGatogary"));
+
+const ContactUs = lazy(() => import("./pages/contactUS/"));
+
+const Products = lazy(() => import("./pages/Products"));
+
+const AboutUS = lazy(() => import("./pages/aboutUS/AboutUS"));
+
+const Gallerys = lazy(() => import("./pages/gallery"));
+
+const NotFoundPage = lazy(() => import("./pages/pageNotFound/"));
+
+const ProductDetails = lazy(() => import("./pages/productDetails"));
+
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Header />
-        <Switch>
+    <Router>
+      <Header />
+      <Switch>
+        <Suspense fallback={<div>Loading...</div>}>
           <Route path="/" exact component={Home} />
           <Route path="/aboutus" exact component={AboutUS} />
           <Route path="/contactus" exact component={ContactUs} />
@@ -42,11 +61,11 @@ const App = () => {
           />
           <Route path="/gallery" exact component={Gallerys} />
           <Route path="/productDetails" exact component={ProductDetails} />
-          <Route path="*" component={NotFoundPage} />
-        </Switch>
-        <Footer />
-      </Router>
-    </div>
+        </Suspense>
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 };
 
