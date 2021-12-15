6ee0b21ad5b04ff6d686df19d8f2580b963330b6
Ali <ali.mohammed4@yahoo.com>
2021-12-15 16:03:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home";
-// import Product from "../src/pages/productGatogary";
-// import ContactUs from "./pages/contactUS/";
-// import Products from "./pages/Products";
-// import AboutUS from "./pages/aboutUS/AboutUS";
-// import Gallerys from "./pages/gallery";
-// import ProductDetails from "./pages/productDetails/";
+import Product from "../src/pages/productGatogary";
+import ContactUs from "./pages/contactUS/";
+import Products from "./pages/Products";
+import AboutUS from "./pages/aboutUS/AboutUS";
+import Gallerys from "./pages/gallery";
+import ProductDetails from "./pages/productDetails/";
 import NotFoundPage from "./pages/pageNotFound/";
 import { Header } from "./components/layouts";
 import { Footer } from "./components/layouts";
@@ -31,17 +31,17 @@ import logo from "./images/logo.webp";
 
 // const Home = lazy(() => import("./pages/home"));
 
-const Product = lazy(() => import("../src/pages/productGatogary"));
+// const Product = lazy(() => import("../src/pages/productGatogary"));
 
-const ContactUs = lazy(() => import("./pages/contactUS/"));
+// const ContactUs = lazy(() => import("./pages/contactUS/"));
 
-const Products = lazy(() => import("./pages/Products"));
+// const Products = lazy(() => import("./pages/Products"));
 
-const AboutUS = lazy(() => import("./pages/aboutUS/AboutUS"));
+// const AboutUS = lazy(() => import("./pages/aboutUS/AboutUS"));
 
-const Gallerys = lazy(() => import("./pages/gallery"));
+// const Gallerys = lazy(() => import("./pages/gallery"));
 
-const ProductDetails = lazy(() => import("./pages/productDetails"));
+// const ProductDetails = lazy(() => import("./pages/productDetails"));
 
 // const NotFoundPage = lazy(() => import("./pages/pageNotFound/"));
 
@@ -57,31 +57,24 @@ const App = () => {
           </div>
         }
       > */}
-
-      <Header />
-
+      <Suspense>
+        <Header />
+      </Suspense>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Suspense
-          fallback={
-            <div className="full-page-loader">
-              <img width="200" src={logo} alt="AIP.Trade logo" />
-            </div>
-          }
-        >
-          <Route path="/aboutus" exact component={AboutUS} />
-          <Route path="/contactus" exact component={ContactUs} />
-          <Route path="/product" exact component={Product} />
-          <Route
-            path="/products/:name"
-            children={<Products />}
-            exact
-            component={Products}
-          />
-          <Route path="/gallery" exact component={Gallerys} />
-          <Route path="/productDetails" exact component={ProductDetails} />
-          <Route path="*" component={NotFoundPage} />
-        </Suspense>
+
+        <Route path="/aboutus" exact component={AboutUS} />
+        <Route path="/contactus" exact component={ContactUs} />
+        <Route path="/product" exact component={Product} />
+        <Route
+          path="/products/:name"
+          children={<Products />}
+          exact
+          component={Products}
+        />
+        <Route path="/gallery" exact component={Gallerys} />
+        <Route path="/productDetails" exact component={ProductDetails} />
+        <Route path="*" component={NotFoundPage} />
       </Switch>
 
       <Footer />
